a7f3b128a039fd9085c1b7f2f5e950b4f5d365d3
rodrigopepato <rodrigo.pepato@gmail.com>
2026-05-13 20:04:07 -0300

    chore: configura CORS para integração com frontend

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'clientes',
     'produtos',
     'vendas',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -53,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -125,3 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+]
