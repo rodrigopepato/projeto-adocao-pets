ce67fe25a21a6ebd1a7d3f6750cafb8385bbbb59
rodrigopepato <rodrigo.pepato@gmail.com>
2026-05-13 19:32:09 -0300

    feat: implementa rotas da API para clientes e produtos

---
+++ backend/produtos/urls.py
@@ -0,0 +1,8 @@
+from rest_framework.routers import DefaultRouter
+
+from .views import ProdutoViewSet
+
+router = DefaultRouter()
+router.register(r'produtos', ProdutoViewSet)
+
+urlpatterns = router.urls
