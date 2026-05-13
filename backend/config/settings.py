12e757ffecec18c26969c6ad5aa687b2283d3d91
rodrigopepato <rodrigo.pepato@gmail.com>
2026-05-13 19:14:45 -0300

    chore: configura conexão do Django com MySQL

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pet_adocao_db',
+        'USER': 'root',
+        'PASSWORD': 'Admin123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
