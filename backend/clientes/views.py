2d123e85f05d8201900e787aae7773200a7e71dc
rodrigopepato <rodrigo.pepato@gmail.com>
2026-05-13 19:29:12 -0300

    feat: adiciona serializers e views da API

--- backend/clientes/views.py
+++ backend/clientes/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Cliente
+from .serializers import ClienteSerializer
+
+
+class ClienteViewSet(viewsets.ModelViewSet):
+    queryset = Cliente.objects.all()
+    serializer_class = ClienteSerializer
--- backend/produtos/views.py
+++ backend/produtos/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Produto
+from .serializers import ProdutoSerializer
+
+
+class ProdutoViewSet(viewsets.ModelViewSet):
+    queryset = Produto.objects.all()
+    serializer_class = ProdutoSerializer
