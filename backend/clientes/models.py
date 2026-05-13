08c3006b217ca6bc70a4417d65e0f6120139b4c7
rodrigopepato <rodrigo.pepato@gmail.com>
2026-05-13 19:25:49 -0300

    feat: adiciona models de clientes e produtos

--- backend/clientes/models.py
+++ backend/clientes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    nome = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=14, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    telefone = models.CharField(max_length=20, blank=True, null=True)
+    endereco = models.CharField(max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return self.nome
